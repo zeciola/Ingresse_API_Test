e7ef40a146e67570c7660801e6eef12377ba8a30
José Ricardo <jr.ciola.bricio@gmail.com>
2019-04-24 22:29:14 -0300

    Update Requests

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_migrate import Migrate
 from .model import configure_db
+from .serializer import congigure_serializer as config_se
 
 
 def create_app():
@@ -8,10 +9,15 @@ def create_app():
     app = Flask(__name__)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/ingresse.db"
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     configure_db(app)
+    config_se(app)
 
     Migrate(app, app.db)
+
+    from .user import blue_print_user
+
+    app.register_blueprint(blue_print_user)
 
     return app
---
+++ app/user.py
@@ -0,0 +1,97 @@
+from flask import Blueprint, jsonify, request, current_app
+from .serializer import UserSchema
+from .model import User
+
+blue_print_user = Blueprint("user", __name__)
+
+# Mostrar todos
+
+
+@blue_print_user.route("/show", methods=["GET"])
+def show():
+
+    result = User.query.all()
+
+    return UserSchema(many=True).jsonify(result), 200
+
+
+# Mostrar por id
+
+
+@blue_print_user.route("/show_by_id/<identificator>", methods=["GET"])
+def show_by_id(identificator):
+
+    result = User.query.filter(User.id == identificator)
+
+    return UserSchema(many=True).jsonify(result), 200
+
+
+# Registar
+
+
+@blue_print_user.route("/register_user", methods=["POST"])
+def create():
+    us = UserSchema()
+
+    user, error = us.load(request.json)
+
+    if error:
+        return jsonify(error), 401
+
+    user.password_hash()
+
+    current_app.db.session.add(user)
+    current_app.db.session.commit()
+
+    return us.jsonify(user), 201
+
+
+# Mudar por id
+
+
+@blue_print_user.route("/change_by_id/<identificator>", methods=["POST"])
+def change(identificator):
+    us = UserSchema()
+    query = User.query.filter(User.id == identificator)
+
+    if us.jsonify(query) != None:
+        return jsonify('Error not found user id')
+
+    query.update(request.json)
+    current_app.db.session.commit()
+
+    return us.jsonify(query.first())
+
+
+# Mudar por id
+
+
+@blue_print_user.route("/change_by_username/<identificator>", methods=["GET","PUT"])
+def change_username(identificator):
+    
+    us = UserSchema()
+    query = User.query.filter(User.username == identificator)
+    if us.jsonify(query) != None:
+        return jsonify('Error not found user')
+    query.update(request.json)
+    current_app.db.session.commit()
+
+    return jsonify(f"The user {identificator} has been modified"), 200
+
+
+# Deletar por id
+
+
+@blue_print_user.route("/delete_by_id/<identificator>", methods=["GET","DELETE"])
+def delete_id(identificator):
+
+    us = UserSchema()
+    query = User.query.filter(User.username == identificator)
+
+    if us.jsonify(query) != None:
+        return jsonify('Error not found user id')
+
+    User.query.filter(User.id == identificator).delete()
+    current_app.db.session.commit()
+
+    return jsonify(f"The user for id: {identificator} has been deleted"), 200
